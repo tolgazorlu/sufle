780d86e8373394c3ea8837e84282e78e424cbfb9
Tolga <tolgazorlu17@gmail.com>
2025-01-02 23:38:30 +0300

    theme ml removed

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -74,7 +74,7 @@ const Navbar = () => {
                     <DropdownMenuSubTrigger>
                       <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
                       <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-                      <span className='ml-2'>Theme</span>
+                      <span className=''>Theme</span>
                     </DropdownMenuSubTrigger>
                     <DropdownMenuPortal>
                       <DropdownMenuSubContent>
